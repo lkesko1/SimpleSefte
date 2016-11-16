18b0cc49cb60a06ee437f51632bbe5922bea7f4d
lkesko1 <lkesko1@etf.unsa.ba>
2016-11-16 21:39:21 +0100

    dodala zatvaranje slike na esc

--- Spirala2/Javascript/OtvoriSliku.js
+++ Spirala2/Javascript/OtvoriSliku.js
@@ -12,3 +12,9 @@ function ZatvoriSliku(){
 	document.getElementById('otvorenaSlikaBox').style.display = "none";
 	/*document.getElementById('odabranaSlika').style.display = "none";*/
 }
+
+document.onkeyup = function (e) {
+    if (e.keyCode == 27) {
+        document.getElementById('otvorenaSlikaBox').style.display = 'none';
+    }
+}
