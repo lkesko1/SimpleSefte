9ea3f906e6b49d0a39e43462927b76c0d338d69c
lkesko1 <lkesko1@etf.unsa.ba>
2016-11-18 12:49:10 +0100

    korekcije

--- Spirala2/Javascript/UcitavanjeStranice.js
+++ Spirala2/Javascript/UcitavanjeStranice.js
@@ -8,11 +8,12 @@ function otvori(stranica) {
         document.getElementsByClassName("glavni")[0].innerHTML = stranica;
 	}
     }
+	var x = document.getElementById("aktivniLink");
+	x.id="";
     if (stranica === 'Pocetna') {
         ajax.open("POST", "pocetna.html", true);
         ajax.send();
-		var x = document.getElementById("aktivniLink");
-		x.id="";
+		
 		var stranica = document.getElementsByTagName('li')[0];
 		var linkStranice = stranica.getElementsByTagName("a")[0];
 		linkStranice.id = "aktivniLink";
@@ -21,8 +22,7 @@ function otvori(stranica) {
     if (stranica === 'Novosti') {
         ajax.open("POST", "novosti.html", true);
         ajax.send();
-		var x = document.getElementById("aktivniLink");
-		x.id="";
+	
 		var stranica = document.getElementsByTagName('li')[1];
 		var linkStranice = stranica.getElementsByTagName("a")[0];
 		linkStranice.id = "aktivniLink";
@@ -30,8 +30,7 @@ function otvori(stranica) {
 	if (stranica === 'Galerija') {
         ajax.open("POST", "galerija.html", true);
         ajax.send();
-		var x = document.getElementById("aktivniLink");
-		x.id="";
+	
 		var stranica = document.getElementsByTagName('li')[2];
 		var linkStranice = stranica.getElementsByTagName("a")[0];
 		linkStranice.id = "aktivniLink";
@@ -39,8 +38,7 @@ function otvori(stranica) {
 	if (stranica === 'Cjenovnik') {
         ajax.open("POST", "cjenovnik.html", true);
         ajax.send();
-		var x = document.getElementById("aktivniLink");
-		x.id="";
+		
 		var stranica = document.getElementsByTagName('li')[3];
 		var linkStranice = stranica.getElementsByTagName("a")[0];
 		linkStranice.id = "aktivniLink";
@@ -49,8 +47,7 @@ function otvori(stranica) {
 	if (stranica === 'Rezervacije') {
         ajax.open("POST", "rez.html", true);
         ajax.send();
-		var x = document.getElementById("aktivniLink");
-		x.id="";
+		
 		var stranica = document.getElementsByTagName('li')[4];
 		var linkStranice = stranica.getElementsByTagName("a")[0];
 		linkStranice.id = "aktivniLink";
@@ -58,12 +55,13 @@ function otvori(stranica) {
 	if (stranica === 'Kontakt') {
         ajax.open("POST", "kontakt.html", true);
         ajax.send();
-		var x = document.getElementById("aktivniLink");
-		x.id="";
+		
 		var stranica = document.getElementsByTagName('li')[5];
 		var linkStranice = stranica.getElementsByTagName("a")[0];
 		linkStranice.id = "aktivniLink";
     }
+	var lista = document.getElementById('meniLista');
+	lista.className = "meniKlasa";
 }
 
 
@@ -74,10 +72,11 @@ function otvoriIndex(stranica) {
             document.getElementsByClassName("glavni")[0].innerHTML = ajax.responseText;
 		if (ajax.readyState == 4 && ajax.status == 404){
 			document.getElementsByClassName("glavni")[0].innerHTML = stranica;
-			var linkovi = document.getElementsByTagName("a");
-            ajax.open("POST", "index.html", true);
-            ajax.send();
-			if (stranica === 'Cjenovnik') ucitaj("Cjenovnik");
 		}
+		var linkovi = document.getElementsByTagName("a");
+        ajax.open("POST", "index.html", true);
+        ajax.send();
+		if (stranica === 'Cjenovnik') otvori('Cjenovnik');
+		
 	}		
 }
